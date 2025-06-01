26d0c0320e092cee5ee7a7b54d33bda959b82463
abdelfettah el ghabaoui <ghabaouiabdelftah@gmail.com>
2025-06-01 11:02:55 +0100

    Configure app for Render deployment and env vars

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,11 +1,12 @@
 # app/config/settings.py
 
-# Clé secrète pour les sessions Flask (changez-la pour une vraie clé forte en production !)
-SECRET_KEY = 'super_secret_key_a_changer_absolument'
+import os # <--- AJOUTEZ CET IMPORT
+
+# Clé secrète pour les sessions Flask
+# Utilise la variable d'environnement SECRET_KEY si elle existe, sinon utilise la valeur par défaut pour le développement local
+SECRET_KEY = os.environ.get('SECRET_KEY', 'super_secret_key_a_changer_absolument')
 
 # Configuration de la base de données PostgreSQL
-# format: 'postgresql://utilisateur:mot_de_passe@host:port/nom_base_de_donnees'
-SQLALCHEMY_DATABASE_URI = 'postgresql://tms_user:tms_pass123@localhost:5432/tms_db'
-SQLALCHEMY_TRACK_MODIFICATIONS = False # Désactive le suivi des modifications des objets SQLAlchemy, économise des ressources
-
-# Assurez-vous de remplacer 'votre_mot_de_passe_secure' par le mot de passe que vous avez défini pour 'tms_user'
+# Utilise la variable d'environnement DATABASE_URL fournie par Render, sinon utilise la valeur locale
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://tms_user:tms_pass123@localhost:5432/tms_db')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
